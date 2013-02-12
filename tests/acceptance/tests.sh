dbe9ebf53bb07994913a353a1347cfad1bc91b73
Jan (yac) Matějka <JMatejka@suse.cz>
2013-02-12 16:03:37 +0100

    automated tests refs #7

---
+++ tests/acceptance/tests.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+base=$(dirname $0)
+cdiff=$base/../../cdiff.py
+PYTHON=${PYTHON:-python}
+
+testNormal() {
+	for i in $base/files/*; do
+		$PYTHON $cdiff --color=always $i/in.diff | diff -b $i/out -
+		assertEquals $? 0
+	done
+}
+
+testSideBySide() {
+	for i in $base/files/*; do
+		$PYTHON $cdiff -s --color=always $i/in.diff | diff -b $i/out.side_by_side -
+		assertEquals $? 0
+	done
+}
+
+
+testNoColorIsUntouched() {
+	for i in $base/files/*; do
+		$PYTHON $cdiff  $i/in.diff | diff $i/in.diff -ub -
+		assertEquals $? 0
+	done
+}
+
+. `which shunit2`
